45edd92c9596ad8566bfecf6bb8819c1aed1e276
leninman <leninmanrique@gmail.com>
2025-08-10 19:17:37 -0400

    Conexion a la bd usando clase Configuration

---
+++ src/main/java/org/aguzman/springcloud/msvc/usuarios/DataSourceConfig.java
@@ -0,0 +1,40 @@
+package org.aguzman.springcloud.msvc.usuarios;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        String rawUrl = System.getenv("MYSQL_URL"); // Ej: mysql://user:pass@host:port/dbname
+        if (rawUrl == null || !rawUrl.startsWith("mysql://")) {
+            throw new RuntimeException("MYSQL_URL no está definido o tiene formato incorrecto");
+        }
+
+        URI uri = URI.create(rawUrl);
+        String[] userInfo = uri.getUserInfo().split(":");
+        String username = userInfo[0];
+        String password = userInfo[1];
+        String jdbcUrl = "jdbc:mysql://" + uri.getHost() + ":" + uri.getPort() + uri.getPath();
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        // Opcional: configuración adicional
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setConnectionTimeout(30000);
+
+        return dataSource;
+    }
+}
+
+
